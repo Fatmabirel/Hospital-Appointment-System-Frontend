f4c49977202938e22014640521181e96f77c002a
Fatma Birel <fatmabrl11@gmail.com>
2024-06-07 18:02:06 +0300

    doctors page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from "./shared/components/footer/footer.component";
-import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { DoctorListComponent } from './features/doctors/components/doctor-list/doctor-list.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, FooterComponent, NavbarComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent],
 })
 export class AppComponent {
   title = 'hospital';
