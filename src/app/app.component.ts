83535a8836634d7debc64f56a22c55538f27ea7c
Fatma Birel <fatmabrl11@gmail.com>
2024-06-07 20:08:23 +0300

    contact page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { DoctorListComponent } from './features/doctors/components/doctor-list/doctor-list.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent, ContactComponent],
 })
 export class AppComponent {
   title = 'hospital';
