99bb578c34a17fa80810c3e511e9098e2d524db6
Fatma Birel <fatmabrl11@gmail.com>
2024-06-07 23:19:56 +0300

    basic-layout added

---
+++ src/app/shared/components/basic-layout/basic-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+@Component({
+  selector: 'app-basic-layout',
+  standalone: true,
+  imports: [CommonModule, FooterComponent, NavbarComponent],
+  templateUrl: './basic-layout.component.html',
+  styleUrl: './basic-layout.component.scss',
+})
+export class BasicLayoutComponent {}
