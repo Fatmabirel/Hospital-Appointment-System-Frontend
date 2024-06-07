660a177670ecd6513108901464a35ec96e70c7eb
ozgurevcmn <102915304+ozgurevcmn@users.noreply.github.com>
2024-06-08 00:10:00 +0300

    hp_body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { DoctorListComponent } from './features/doctors/components/doctor-list/doctor-list.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { HpBodyComponent } from './shared/components/hp_body/hp_body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent, ContactComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent, ContactComponent, HpBodyComponent],
 })
 export class AppComponent {
   title = 'hospital';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { HpBodyComponent } from './shared/components/hp_body/hp_body.component';
 
 export const routes: Routes = [
-    
+    {
+        path: 'shared/components',
+        component: HpBodyComponent
+    }
 ];
