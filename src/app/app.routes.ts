c4bfd731170a4014f1c1c8a20bd258cff57051ee
Okan taşkıran <okan.taskiran14@gmail.com>
2024-06-08 19:43:27 +0300

    about-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,21 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
+import { HomePageComponent } from './routes/home-page/home-page/home-page.component';
 import { AboutComponent } from './features/about/about.component';
 
 
 export const routes: Routes = [
   {
-    path:'features/about',
+    path:'about',
     component: AboutComponent
+  },
+  {
+
+    path: '', // Route belirtilen path ile eşleştiğinde
+    component: HomePageComponent, // İlgili
   }
 
 
 ];
-=======
-import { HomePageComponent } from './routes/home-page/home-page/home-page.component';
 
-export const routes: Routes = [{
-    path: '', // Route belirtilen path ile eşleştiğinde
-    component: HomePageComponent, // İlgili componenti AppComponent'ten başlayarak
-    // ilk karşılaştığı <router-outlet></router-outlet> etiketine yerleştirir.
-  },];
->>>>>>> origin
+
+
