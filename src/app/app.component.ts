b0e7b67aa7f7393ecd5d016885ea21619c28570f
ozgurevcmn <102915304+ozgurevcmn@users.noreply.github.com>
2024-06-09 02:07:58 +0300

    homepage commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,14 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { DoctorListComponent } from './features/doctors/components/doctor-list/doctor-list.component';
 import { ContactComponent } from './features/contact/contact.component';
-import { HpBodyComponent } from './shared/components/hp_body/hp_body.component';
+import { HomePageComponent } from './routes/home-page/home-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent, ContactComponent, HpBodyComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent,DoctorListComponent, ContactComponent, HomePageComponent],
 })
 export class AppComponent {
   title = 'hospital';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,10 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { HpBodyComponent } from './shared/components/hp_body/hp_body.component';
+import { HomePageComponent } from './routes/home-page/home-page.component';
 
 export const routes: Routes = [
     {
-        path: 'shared/components',
-        component: HpBodyComponent
+        path: '',
+        //component: HpBodyComponent,
+        component: HomePageComponent
     }
 ];
-=======
-import { HomePageComponent } from './routes/home-page/home-page/home-page.component';
-
-export const routes: Routes = [{
-    path: '', // Route belirtilen path ile eşleştiğinde
-    component: HomePageComponent, // İlgili componenti AppComponent'ten başlayarak
-    // ilk karşılaştığı <router-outlet></router-outlet> etiketine yerleştirir.
-  },];
->>>>>>> origin/master
