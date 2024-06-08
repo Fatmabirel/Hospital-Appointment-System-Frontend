0c11f15a9b07fdfed8993e35189bba95a3dab544
Okan taşkıran <okan.taskiran14@gmail.com>
2024-06-09 01:02:35 +0300

    about.component

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
+import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,BasicLayoutComponent,RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- src/app/shared/components/basic-layout/basic-layout.component.ts
+++ src/app/shared/components/basic-layout/basic-layout.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { AboutComponent } from '../../../features/about/about.component';
 @Component({
   selector: 'app-basic-layout',
   standalone: true,
-  imports: [CommonModule, FooterComponent, NavbarComponent],
+  imports: [CommonModule, FooterComponent, NavbarComponent,AboutComponent],
   templateUrl: './basic-layout.component.html',
   styleUrl: './basic-layout.component.scss',
 })
