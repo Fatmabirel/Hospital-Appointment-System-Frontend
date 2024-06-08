7d4e52b18a6cc20d52ab3937b92c0f58f436ef75
Okan taşkıran <okan.taskiran14@gmail.com>
2024-06-08 19:24:17 +0300

    about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './features/about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path:'features/about',
+    component: AboutComponent
+  }
+
+
+];
