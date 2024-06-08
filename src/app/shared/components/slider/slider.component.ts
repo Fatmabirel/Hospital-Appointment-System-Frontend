80ebb65c588308a031a1e30b700114c28ad44469
mervekaratass <72658817+mervekaratass@users.noreply.github.com>
2024-06-08 20:05:59 +0300

    edit slider_css and branch_css

--- src/app/shared/components/slider/slider.component.ts
+++ src/app/shared/components/slider/slider.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasicLayoutComponent } from "../basic-layout/basic-layout.component";
 
 @Component({
-  selector: 'app-slider',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './slider.component.html',
-  styleUrl: './slider.component.scss'
+    selector: 'app-slider',
+    standalone: true,
+    templateUrl: './slider.component.html',
+    styleUrl: './slider.component.scss',
+    imports: [CommonModule]
 })
 export class SliderComponent {
 
