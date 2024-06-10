672784dea4232d8a6e6a29690a3a8ca716daf3fb
Sinem Girit <sinemdursun106@gmail.com>
2024-06-10 23:32:29 +0300

    slider

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
 import { RouterModule } from '@angular/router';
+import { SliderComponent } from '../../shared/components/slider/slider.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [BasicLayoutComponent, RouterModule],
+  imports: [BasicLayoutComponent, RouterModule,SliderComponent,],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
