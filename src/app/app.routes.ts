9c65e34ff50111bbc2ddc793ea3380dfc0dc79d4
ozgurevcmn <102915304+ozgurevcmn@users.noreply.github.com>
2024-06-13 00:40:10 +0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './routes/home-page/home-page.component';
 import { BranchListComponent } from './features/branches/components/branch-list/branch-list.component';
 import { DoctorListComponent } from './features/doctors/components/doctor-list/doctor-list.component';
 import { ContactComponent } from './features/contact/contact.component';
+import { AboutComponent } from './features/about/about.component';
 
 
 export const routes: Routes =
