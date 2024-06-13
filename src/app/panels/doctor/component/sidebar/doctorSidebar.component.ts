b9150f73c645ed853f4246296a83fbc4a96281fa
Fatma Birel <fatmabrl11@gmail.com>
2024-06-13 16:49:45 +0300

    jwt_decode

--- src/app/panels/doctor/component/sidebar/doctorSidebar.component.ts
+++ src/app/panels/doctor/component/sidebar/doctorSidebar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DoctorService } from '../../../../features/doctors/services/doctor.service';
 
 @Component({
   selector: 'app-doctor-sidebar',
@@ -9,7 +10,24 @@ import { RouterModule } from '@angular/router';
     CommonModule,RouterModule
   ],
   templateUrl: './doctorSidebar.component.html',
-  styleUrl: './doctorSidebar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './doctorSidebar.component.scss'
 })
-export class DoctorSidebarComponent { }
+export class DoctorSidebarComponent implements OnInit {
+  doctorName: string = '';
+  doctorId: string = '';
+
+  constructor(private doctorService: DoctorService) {}
+
+  ngOnInit(): void {
+    this.doctorService.getDoctorProfile().subscribe(
+      (doctor) => {
+        this.doctorName = `${doctor.firstName} ${doctor.lastName}`;
+        console.log(this.doctorName);
+      },
+      (error) => {
+        console.error('Hata:', error);
+      }
+    );
+  }
+
+}
