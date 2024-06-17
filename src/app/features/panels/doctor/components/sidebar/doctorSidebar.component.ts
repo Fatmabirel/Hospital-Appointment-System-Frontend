dcaf165f3fe075b75b0eca6faabe503ba78fc218
Fatma Birel <fatmabrl11@gmail.com>
2024-06-17 17:16:12 +0300

    logout operations

--- src/app/features/panels/doctor/components/sidebar/doctorSidebar.component.ts
+++ src/app/features/panels/doctor/components/sidebar/doctorSidebar.component.ts
@@ -1,25 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { DoctorService } from '../../../../doctors/services/doctor.service';
 import { Doctor } from '../../../../doctors/models/doctor';
+import { AuthService } from '../../../../../core/auth/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-doctor-sidebar',
   standalone: true,
-  imports: [
-    CommonModule,RouterModule
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './doctorSidebar.component.html',
-  styleUrl: './doctorSidebar.component.scss'
+  styleUrl: './doctorSidebar.component.scss',
 })
 export class DoctorSidebarComponent implements OnInit {
   doctor: Doctor;
-  doctorName:string = '';
-  doctorTitle:string = '';
+  doctorName: string = '';
+  doctorTitle: string = '';
   errorMessage: string;
 
-  constructor(private doctorService: DoctorService) {}
+  constructor(
+    private doctorService: DoctorService,
+    private authService: AuthService,
+    private toastrService:ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.doctorService.getDoctorProfile().subscribe(
@@ -35,5 +40,9 @@ export class DoctorSidebarComponent implements OnInit {
       }
     );
   }
-
+  logout(): void {
+    this.authService.logout();
+    this.toastrService.success('Başarıyla çıkış yaptınız. Giriş sayfasına yönlendiriliyorsunuz', 'Başarılı');
+    this.router.navigate(['/']); // Giriş sayfasına yönlendir
+  }
 }
