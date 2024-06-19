0037e153f8766972b7e212383d4c6e0bba3be424
Fatma Birel <fatmabrl11@gmail.com>
2024-06-19 20:58:06 +0300

    admin sidebar profile field

--- src/app/features/panels/admin/components/sidebar/adminSidebar.component.ts
+++ src/app/features/panels/admin/components/sidebar/adminSidebar.component.ts
@@ -1,15 +1,32 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TokenService } from '../../../../../core/auth/services/token.service';
+import { AdminService } from '../../../../admins/services/admin.service';
+import { Admin } from '../../../../admins/models/admin';
 
 @Component({
   selector: 'app-admin-sidebar',
   standalone: true,
-  imports: [
-    CommonModule,RouterModule
-  ],
+  imports: [CommonModule, RouterModule],
   templateUrl: './adminSidebar.component.html',
   styleUrl: './adminSidebar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminSidebarComponent { }
+export class AdminSidebarComponent implements OnInit {
+  admin: Admin;
+  adminName: string = '';
+
+  constructor(private tokenService: TokenService,private adminService:AdminService) {}
+
+  ngOnInit(): void {
+   this.getAdminProfile();
+  }
+
+  getAdminProfile(){
+    this.adminService.getAdminProfile().subscribe((admin) => {
+      this.adminName = admin.firstName + ' ' + admin.lastName;
+      return this.adminName;
+    })
+  }
+
+}
