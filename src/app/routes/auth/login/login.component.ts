47652ee3ec1af5c881c62677e5fe9c8ad8d68e87
mervekaratass <72658817+mervekaratass@users.noreply.github.com>
2024-07-03 00:45:04 +0300

    edit login component

--- src/app/routes/auth/login/login.component.ts
+++ src/app/routes/auth/login/login.component.ts
@@ -50,9 +50,8 @@ export class LoginComponent implements OnInit {
 
       email:new FormControl ('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(8),Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\\d!@#$%^&*(),.?":{}|<>]{8,15}$')]),
-      authenticatorCode: [''],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+
+
 
     });
   }
